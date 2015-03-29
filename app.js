cb7c33e3ee4aaad0628b89fb09c7c7ec61c6c389
stedes <stevendesair@gmail.com>
2015-03-29 21:44:43 +0200

    starting using ui router

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+
 
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
+
+  $scope.posts = posts.posts;
 
   $scope.posts=[
     {title:"post1", upvotes:3},
     {title:"post2", upvotes:5}
   ]
 
+
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title   : $scope.title,
+      link    : $scope.link,
+      upvotes : 0});
     $scope.title = '';
   };
 
